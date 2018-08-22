ef32569425c615f24b4cb556a1ada848aa866c1a
Praval Jain <praval41195jain@gmail.com>
2018-08-22 13:15:10 +0530

    ooglemap

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,17 @@
-const API_key = 'b6907d289e10d714a6e88b30761fae22';
-const ROOT_URL = `https://samples.openweathermap.org/data/2.5/forecast?appid=${API_key}`;
+import axios from 'axios';
+
+const API_key = '275a40e07218c57c86eac371036efd52';
+const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_key}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER'
 
-export function fetchWeather(city) {
+export function fetchWeather(city) { 
+
     const url = `${ROOT_URL}&q=${city},us`;
-    
+    const request  = axios.get(url);
+     
     return {
-        type : FETCH_WEATHER
+        type : FETCH_WEATHER,
+        payload : request
     };
 }
